bf3230d02260a2e003caa4d431491cc7bae4d756
Shobande James Oluwaseun <shobandejames@gmail.com>
2024-07-17 18:49:01 +0100

    final netlify deployment settings

--- config/dbConn.js
+++ config/dbConn.js
@@ -4,7 +4,9 @@ const connectDB = async() => {
     try {
         await mongoose.connect(process.env.DATABASE_URI, {
             useUnifiedTopology: true, 
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            serverSelectionTimeoutMS: 30000, // Increase the timeout to 30 seconds
+            socketTimeoutMS: 60000, // Close sockets after 45 seconds of inactivity
         })
     } catch (error) {
         console.log(error)
